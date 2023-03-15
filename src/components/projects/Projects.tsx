cc6266bf7ced48d6f5be54bc231843dfa11d6a82
Muhammad Anas <55958631+MuhammadAnas-01@users.noreply.github.com>
2023-03-15 16:48:36 +0500

    Update Projects.tsx

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
         <p className="text-xl trakcing-widest uppercase text-[#5651e5]">
           Projects
         </p>
-        <h2 className="py-4">What I've Build</h2>
+        <h2 className="py-4">What I have Build</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
             <Image
