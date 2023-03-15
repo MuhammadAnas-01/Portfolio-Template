fcc1e701a3c94fd8abcbb43df9dae44b47d2942c
Muhammad Anas <55958631+MuhammadAnas-01@users.noreply.github.com>
2023-03-15 16:48:01 +0500

    Update Hero.tsx

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -11,17 +11,17 @@ const Hero = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET'S BUILD SOMETHING
+            LETS BUILD SOMETHING
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi,I m <span className="text-[#5651e5]">Anas</span>
+            Hi,I am <span className="text-[#5651e5]">Anas</span>
           </h1>
           <h1 className="py-4 text-gray-700">
             A Front-End Web Developer
           </h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            I'm font-end web developer specializing in building (and ocasionally
-            designing) exceptional digital experiences. Currently, I'm focused
+            I am font-end web developer specializing in building (and ocasionally
+            designing) exceptional digital experiences. Currently, I am focused
             on building responsive front-wnd web applications while learning
             back-end technologies.
           </p>
